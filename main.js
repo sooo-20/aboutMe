03c166d818c2d1aea84f51994ccebcb267788e1b
sooo-20 <69193892+sooo-20@users.noreply.github.com>
2020-08-14 19:39:57 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -4,3 +4,25 @@ const menu = document.querySelector(".navbar_menu");
 toggleBtn.addEventListener("click", () => {
   menu.classList.toggle("active");
 });
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
