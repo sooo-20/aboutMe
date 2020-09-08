c408fe617facde97bfeb41b3b493ad2d353f1216
sooo-20 <sojeong.heo@gmail.com>
2020-09-08 17:31:17 +0900

    First Commit

--- main.js
+++ main.js
@@ -1,28 +1,30 @@
-const toggleBtn = document.querySelector(".navbar_toggleBtn");
-const menu = document.querySelector(".navbar_menu");
+//네비게이션 모바일화면 토글버튼
+const toggleBtn = document.querySelector('.navbar_toggleBtn');
+const menu = document.querySelector('.navbar_menu');
 
-toggleBtn.addEventListener("click", () => {
-  menu.classList.toggle("active");
+toggleBtn.addEventListener('click', () => {
+  menu.classList.toggle('active');
 });
 
+//사진 슬라이드
 var slideIndex = 0;
 showSlides();
 
 function showSlides() {
   var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
+  var slides = document.getElementsByClassName('mySlides');
+  var dots = document.getElementsByClassName('dot');
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+    slides[i].style.display = 'none';
   }
   slideIndex++;
   if (slideIndex > slides.length) {
     slideIndex = 1;
   }
   for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" current", "");
+    dots[i].className = dots[i].className.replace(' current', '');
   }
-  slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " current";
-  setTimeout(showSlides, 1800); // Change image every 2 seconds
+  slides[slideIndex - 1].style.display = 'block';
+  dots[slideIndex - 1].className += ' current';
+  setTimeout(showSlides, 1800);
 }
